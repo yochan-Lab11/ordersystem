322189093a20dd6fff0a6dd5ca82fc0165eed31c
yochan-Lab11 <150769599+yochan-Lab11@users.noreply.github.com>
2023-11-26 16:19:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function onSubmit() {
+/*function onSubmit() {
     const name = document.getElementById('answer').value;
     const msg = `${name}`;
     console.log(msg);
     sendText(msg);
     return false;
-}
+}*/
+
+$(function () {
+            $('form').submit(function () {
+                const name    = $('input[name="name"]').val();
+                const bangou = $('input[name="bangou"]').val();
+                const shushoujou  = $('input[name="shushoujou"]').val();
+                const keika  = $('input[name="keika"]').val();
+                const hatunetu = $('input[name="hatunetu"]').val();
+                const msg = `${name}\n${bangou}\n${shushoujou}\n${keika}\n${hatunetu}`;
+                sendText(msg);
+                return false;
+            });
+        });
