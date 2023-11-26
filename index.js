3a2a59811c1a8b08bebee319274666240b60ce59
yochan-Lab11 <150769599+yochan-Lab11@users.noreply.github.com>
2023-11-26 15:00:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function onSubmit() {
     var name = document.getElementById('answer1').value;
-    var count = document.getElementById('answer2').value;
-    var bento = document.getElementById('answer3').value;
-    const msg = "【送信内容】\n" + text1;
+    //var count = document.getElementById('answer2').value;
+    //var bento = document.getElementById('answer3').value;
+    const msg = "【送信内容】\n" + name;
     console.log(msg);
     sendText(msg);
     return false;
