953d62d6a603c9bcc58e2005c8941644c33f89bb
yochan-Lab11 <150769599+yochan-Lab11@users.noreply.github.com>
2023-11-26 16:20:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ $(function () {
             $('form').submit(function () {
                 const name    = $('input[name="name"]').val();
                 const bangou = $('input[name="count"]').val();
-                const shushoujou  = $('input[name="bento"]').val();
-                const keika  = $('input[name="keika"]').val();
-                const hatunetu = $('input[name="hatunetu"]').val();
+                const shushoujou  = $('input[name="menu"]').val();
                 const msg = `${name}\n${bangou}\n${shushoujou}\n${keika}\n${hatunetu}`;
                 sendText(msg);
                 return false;
