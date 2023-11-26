506e6e1fdd0b31949ec326261181ec0c1fdefd07
yochan-Lab11 <150769599+yochan-Lab11@users.noreply.github.com>
2023-11-26 14:02:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function onSubmit(){
     const text1 = document.getElementById('answer1').value;
     const text2 = document.getElementById('answer2').value;
-    const text1 = document.getElementById('answer3').value;
+    const text3 = document.getElementById('answer3').value;
     
     const msg = "【送信内容】\n" + text1 "\n" +  text2 "\n" + text3 "\n" ;
     console.log(msg);
